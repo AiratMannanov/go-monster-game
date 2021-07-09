4b1a615544858adb7f0a2ff24619abf8745314d8
Airat <parkourtes@gmail.com>
2021-07-09 23:27:55 +0300

    add metadata

--- acitons/acitons.go
+++ acitons/acitons.go
@@ -7,17 +7,42 @@ import (
 
 var randSource = rand.NewSource(time.Now().UnixNano())
 var randGenerator = rand.New(randSource)
+var currentMonsterHealth = MONSTER_HEALTH
+var currentPlayerHealth = PLAYER_HEALTH
 
-func AttackMonster(isSpecialRound bool) {
+func AttackMonster(isSpecialAttack bool) {
+	minAttachValue := PLAYER_ATTACK_MIN_DMG
+	maxAttackValue := PLAYER_ATTACK_MAX_DMG
 
+	if isSpecialAttack {
+		minAttachValue = PLAYER_SPECIAL_ATTACK_MIN_DMG
+		maxAttackValue = PLAYER_SPECIAL_ATTACK_MAX_DMG
+	}
+	dmgValue := generateRandBetween(minAttachValue, maxAttackValue)
+	currentMonsterHealth -= dmgValue
 }
 
 func HealPlayer() {
+	minAttachValue := PLAYER_HEAL_MIN_VALUE
+	maxAttackValue := PLAYER_HEAL_MAX_VALUE
 
+	healValue := generateRandBetween(minAttachValue, maxAttackValue)
+
+	healthDiff := PLAYER_HEALTH - currentMonsterHealth
+
+	if healthDiff >= healValue {
+		currentPlayerHealth += healValue
+	} else {
+		currentPlayerHealth = PLAYER_HEALTH
+	}
 }
 
 func AttackPlayer() {
+	minAttachValue := MONSTER_ATTACK_MIN_DMG
+	maxAttackValue := MONSTER_ATTACK_MAX_DMG
 
+	dmgValue := generateRandBetween(minAttachValue, maxAttackValue)
+	currentPlayerHealth -= dmgValue
 }
 
 func generateRandBetween(min int, max int) int {
